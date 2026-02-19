01f29f20850545828d06277a25da1dbbe5a452a0
Subodh <dynocoder1@gmail.com>
2026-02-19 12:12:33 +0530

    feat: add Project model and update Message table; implement project creation and association in TRPC procedures

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
+import { projectsRouter } from '@/modules/projects/server/procedures';
 import { createTRPCRouter } from '../init';
 import { MessagesRouter } from '@/modules/messages/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
   messages: MessagesRouter,
+  projects: projectsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
