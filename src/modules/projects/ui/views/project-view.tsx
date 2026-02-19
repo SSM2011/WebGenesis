a20cfffdf2a208a7c81ed9382b0bafd5017bc430
Subodh <dynocoder1@gmail.com>
2026-02-19 19:28:51 +0530

    feat: add Hint component for tooltips; integrate into FragmentWeb and ProjectView for enhanced user guidance

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -9,6 +9,7 @@ import { MessagesContainer } from "../components/messages-container";
 import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma/client";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 
 interface Props {
@@ -43,7 +44,7 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={65}
                     minSize={50}
                 >
-                    TODO: Preview
+                    {!!activeFragment &&<FragmentWeb data={activeFragment}/>  }
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
