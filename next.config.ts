8ce13ceabb93a5e33474a3507dbb232142ac200c
Subodh <dynocoder1@gmail.com>
2026-02-24 20:12:43 +0530

    feat: add Docker support with Dockerfile and docker-compose.yml

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // IMPORTANT for Docker
+  // output: "standalone",
 };
 
 export default nextConfig;
